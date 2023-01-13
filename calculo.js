a4b83f2954c63808994f02d82f8c0a4ac1c9c027
frodriguez84 <fernando.rodriguez84@yahoo.com.ar>
2023-01-12 21:30:54 -0300

    conversor

---
+++ calculo.js
@@ -0,0 +1,44 @@
+var usdCol, usdArg, col;
+
+fetch('https://www.datos.gov.co/resource/32sa-8pi3.json')
+  .then(response => response.json())
+  .then(data => {
+
+    data = data[0].valor
+    usdCol = data
+    //let value = "Hello World";
+    document.getElementById("demo").innerHTML = data;
+    console.log(data);
+
+  });
+
+
+usdArg = 400;
+ 
+function colToUsd(col) {
+  return col / usdCol;
+}
+
+function usdToArg(usd) {
+  return usd * usdArg;
+}
+
+function processForm(event) {
+  
+  event.preventDefault();
+
+ // var inputUsdCol = document.getElementById("dolCol");
+  //usdCol = inputUsdCol.value
+  var inputElement = document.getElementById("col");
+  col = inputElement.value;
+  var final = usdToArg(colToUsd(col));
+  console.log(final);
+
+  var resultElement = document.getElementById("result");
+  resultElement.textContent = "Pesos: $" + Math.trunc(final).toLocaleString();
+}
+
+var formElement = document.getElementById("conversion-form");
+formElement.addEventListener("submit", processForm);
+
+
